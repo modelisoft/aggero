2dde2d376d80fcfdca21ddfaa42ccd8afc8a6ffb
aakar <ali.akar82@gmail.com>
2014-11-15 12:39:52 +0100

    Share project "com.modelisoft.aggero.featuremodel"

---
+++ plugins/com.modelisoft.aggero.featuremodel/src/com/modelisoft/aggero/featuremodel/internal/Activator.java
@@ -0,0 +1,84 @@
+/**
+ * <copyright>
+ *
+ * Copyright (c) Modelisoft and others.
+ * All rights reserved.
+ *
+ * Contributors:
+ *     Modelisoft - Initial API and implementation
+ *
+ * </copyright>
+ */
+package com.modelisoft.aggero.featuremodel.internal;
+
+import org.eclipse.emf.common.EMFPlugin;
+import org.eclipse.emf.common.util.ResourceLocator;
+
+/**
+ * This is the central singleton for this plug-in.
+ */
+public final class Activator extends EMFPlugin {
+
+	/**
+	 * Keep track of the singleton.
+	 */
+	public static final Activator INSTANCE = new Activator();
+
+	/**
+	 * Keep track of the singleton.
+	 */
+	private static Implementation plugin;
+
+	/**
+	 * Create the instance.
+	 */
+	public Activator() {
+		super(new ResourceLocator[] {});
+	}
+
+	/**
+	 * Returns the singleton instance of the Eclipse plug-in.
+	 *
+	 * @return the singleton instance.
+	 */
+	@Override
+	public ResourceLocator getPluginResourceLocator() {
+		return plugin;
+	}
+
+	/**
+	 * Returns the singleton instance of the Eclipse plug-in.
+	 *
+	 * @return the singleton instance.
+	 */
+	public static Implementation getPlugin() {
+		return plugin;
+	}
+
+	/**
+	 * Returns the singleton instance of the Eclipse plug-in. This method does actually the same thing as getPlugin()
+	 * and has been put in place for compatibility reasons with Activator classes which are not EMF-based but generated
+	 * by PDE.
+	 *
+	 * @return the singleton instance.
+	 */
+	public static Implementation getDefault() {
+		return plugin;
+	}
+
+	/**
+	 * The actual implementation of the Eclipse <b>Plug-in</b>.
+	 */
+	public static class Implementation extends EclipsePlugin {
+
+		/**
+		 * Creates an instance.
+		 */
+		public Implementation() {
+			super();
+
+			// Remember the static instance
+			plugin = this;
+		}
+	}
+}
